3b9eb3c1ee0391c1c0af85e7a06649d4a9c52e52
H.K. An <32886925+HK-An@users.noreply.github.com>
2025-05-10 19:28:10 +0900

    Time: 0 ms (100%), Space: 42.4 MB (26.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,44 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int mergedIdx = m + n;
-        m--;
-        n--;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        for(int i = mergedIdx-1; i >= 0; i--){
-            if(m < 0) {
-                nums1[i] = nums2[n];
-                n--;
-                continue;
-            } 
-            if(n < 0) {
-                nums1[i] = nums1[m];
-                m--;
-                continue;
-            }
-
-            if(nums1[m] > nums2[n]) {
-                nums1[i] = nums1[m];
-                m--;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] < nums2[j]) {
+                nums1[k--] = nums2[j--];
             } else {
-                nums1[i] = nums2[n];
-                n--;
+                nums1[k--] = nums1[i--];
             }
         }
+
+        while(j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+        // int mergedIdx = m + n;
+        // m--;
+        // n--;
+
+        // for(int i = mergedIdx-1; i >= 0; i--){
+        //     if(m < 0) {
+        //         nums1[i] = nums2[n];
+        //         n--;
+        //         continue;
+        //     } 
+        //     if(n < 0) {
+        //         nums1[i] = nums1[m];
+        //         m--;
+        //         continue;
+        //     }
+
+        //     if(nums1[m] > nums2[n]) {
+        //         nums1[i] = nums1[m];
+        //         m--;
+        //     } else {
+        //         nums1[i] = nums2[n];
+        //         n--;
+        //     }
+        // }
+
     }
 }
