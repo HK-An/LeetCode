9a1c0fb9e15e626e710da246eed8beb160174d35
H.K. An <32886925+HK-An@users.noreply.github.com>
2025-05-10 19:28:25 +0900

    Time: 0 ms (100%), Space: 42.4 MB (42.36%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -15,30 +15,5 @@ class Solution {
         while(j >= 0) {
             nums1[k--] = nums2[j--];
         }
-        // int mergedIdx = m + n;
-        // m--;
-        // n--;
-
-        // for(int i = mergedIdx-1; i >= 0; i--){
-        //     if(m < 0) {
-        //         nums1[i] = nums2[n];
-        //         n--;
-        //         continue;
-        //     } 
-        //     if(n < 0) {
-        //         nums1[i] = nums1[m];
-        //         m--;
-        //         continue;
-        //     }
-
-        //     if(nums1[m] > nums2[n]) {
-        //         nums1[i] = nums1[m];
-        //         m--;
-        //     } else {
-        //         nums1[i] = nums2[n];
-        //         n--;
-        //     }
-        // }
-
     }
 }
