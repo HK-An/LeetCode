6ff9e5e0adc13c002407d5e2975b5993110b1493
H.K. An <32886925+HK-An@users.noreply.github.com>
2025-05-11 00:14:11 +0900

    Time: 2 ms (83.61%), Space: 46.3 MB (10.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,14 +4,12 @@ class Solution {
 
         int pointer = 0;
         for(int i = 0; i < nums.length; i++) {
-            if(i == 0 && nums[i] != 0) {
+            if(nums[i] != 0) {
+                if(i != pointer) {
+                    nums[pointer] = nums[i];
+                    nums[i] = 0;
+                }
                 pointer++;
-                continue;
-            }
-            else if(nums[i] != 0) {
-                int temp = nums[pointer];
-                nums[pointer++] = nums[i];
-                nums[i] = temp;
             } 
         }
     }
